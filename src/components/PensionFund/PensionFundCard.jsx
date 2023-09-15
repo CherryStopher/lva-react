f63abf113435a8693c6b6d93e951e55da655e10d
Cristobal Saldias <cristobals80@hotmail.com>
2023-09-14 23:30:35 -0300

    order some css

--- src/components/PensionFund/PensionFundCard.jsx
+++ src/components/PensionFund/PensionFundCard.jsx
@@ -8,15 +8,14 @@ function PensionFundCard({ fundName, fund }) {
   return (
     <div key={fundName} className={styles.fund}>
       <h3 className={styles.fundTitle}>{fundName}</h3>
-      <div className={styles.fundStats}>
-        {Object.keys(fund).map((key) => (
-          <div className={styles.fundStat} key={key}>
-            <p>{key}:</p>
-            <ProgressBar value={fund[key]} maxValue={MAX_VALUE} />
-            <div className={styles.fundValue}>{fund[key]}</div>
-          </div>
-        ))}
-      </div>
+
+      {Object.keys(fund).map((key) => (
+        <div className={styles.fundStat} key={key}>
+          <p>{key}:</p>
+          <ProgressBar value={fund[key]} maxValue={MAX_VALUE} />
+          <div className={styles.fundValue}>{fund[key]}</div>
+        </div>
+      ))}
     </div>
   );
 }
